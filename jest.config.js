eee13bb2b32a89cd6f221a19910e8b2f7189876c
thomaslynn132 <thantlinnaing0000@gmail.com>
2026-02-10 11:37:19 +0630

    remove example test suite and jest configuration files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export const testEnvironment = "node";
-export const transform = {};
-export const testMatch = ["**/*.test.js", "**/*.spec.js"];
-export const collectCoverage = true;
-export const collectCoverageFrom = ["**/*.{js,ts}", "!**/node_modules/**", "!coverage/**"];
-export const coverageDirectory = "coverage";
-export const coverageReporters = ["json-summary", "text", "lcov"];
-export const verbose = true;
