e2e5ac0d7e6ea0250882f5a25cecf9db4aacee17
thomaslynn132 <thantlinnaing0000@gmail.com>
2026-02-10 11:47:16 +0630

    prepare to deploy

--- test.js
+++
@@ -1,5 +0,0 @@
-function hello() {
-  return "Hello PR!";
-}
-
-export default { hello };
--- test.spec.js
+++
@@ -1,5 +0,0 @@
-import { hello } from "./test";
-
-test("hello returns correct string", () => {
-  expect(hello()).toBe("Hello PR!");
-});
